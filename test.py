9986d477a7f588183e56702c56cf5f963a7b9cf1
Akshaya <138713862+21R01A0524@users.noreply.github.com>
2025-07-21 17:18:53 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,80 @@
+"""print("Hello Python")"""#printing hello python
+"""a=10
+b=19
+print(a+b)
+print(a/b)"""#addition and division
+
+"""a=int(input())
+b=int(input())
+print(a+b)
+print(a/b)"""#by user input
+"""
+base=int(input())
+height=int(input())
+area=0.5*base*height
+print(area)"""#area of triangle
+"""
+a=int(input())
+b=int(input())
+a=a-b
+b=a+b
+a=b-a
+print(a,b)"""#swaping of 2 numbers using arthematic operations
+
+"""a=int(input())
+b=int(input())
+a=a^b
+b=a^b
+a=b^a
+print(a,b)"""#using xor functions
+
+#area and perimeter of
+#square
+"""a=int(input())
+area=a*a
+perimeter=4*a
+print("area of square is:",area)
+print("perimeter of square is:",perimeter)"""
+
+#rectangle
+a=int(input())
+b=int(input())
+area=a*b
+perimeter=2*(a+b)
+print("area of rectangle is:",area)
+print("perimeter of rectangle is:",perimeter)
+
+#triangle
+"""base=int(input())
+height=int(input())
+area=0.5*base*height
+perimeter=base+height+base
+print("area of triangle is:",area)
+print("perimeter of triangle is:",perimeter)
+
+#circle
+radius=int(input())
+area=3.14*radius
+perimeter=2*3.14*radius
+print("area of circle is:",area)
+print("perimeter of circle:",perimeter)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
