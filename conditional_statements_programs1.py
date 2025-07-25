cac110aaf39f09ab3609caa3310c02fab62f93d9
Akshaya <138713862+21R01A0524@users.noreply.github.com>
2025-07-25 18:38:20 +0530

    Add files via upload

---
+++ conditional_statements_programs1.py
@@ -0,0 +1,153 @@
+#number is 0 or not
+"""n=int(input())
+if n==0:
+    print("the i/p is correct")
+print("done")
+#0
+the i/p is correct
+done
+#1
+done
+"""
+#vote eligibility
+"""
+n=int(input())
+if (n>=18):
+    print("the person is eligible")
+else:
+    print(f'{n} years is not eligible')
+
+"""
+
+#WAP to check whether the number is positive or negative
+"""
+n=int(input())
+if n>=0:
+    print(f'{n} is positive')
+else:
+    print(f'{n} is negative')
+"""
+#check whether the number is even or odd
+"""
+n=int(input())
+if(n%2==0):
+    print(f'{n} is even')
+else:
+    print(f'{n} is odd')
+"""
+
+#find the greater of 2 numbers
+"""
+n=int(input())
+m=int(input())
+print(max(n,m))
+"""
+#find the largest among 3 numbers(using built in functions)
+"""
+x=int(input())
+y=int(input())
+z=int(input())
+print(max(x,y,z))
+"""
+#find the largest among 3 numbers(with out using built in functions)
+"""
+x,y,z=map(int,input().split())
+x=int(input())
+y=int(input())
+z=int(input())
+"""
+"""
+if(x>=y) and (x>=z):
+    print(f'{x} is greater')
+if(y>=z) and (y>=x):
+    print(f'{y} is greater')
+else:
+    print(f'{z} is greater')
+"""
+#check if a number is divisible by 5 and 11
+"""
+n=int(input())
+if (n%5==0 and n%11==0):
+    print(f'{n} is divisible by both 5 and 11')
+else:
+    print(f'{n} is not divisible by both 5 and 11')
+"""
+#check whether it is leap year or not
+"""
+ n=int(input())
+if (n%4==0 and n%100!=0) or n%400==0 :
+    print(f'{n} is a leap year')
+else:
+    print(f'{n} is not a leap year')   
+"""
+#check whether a character is vowel or a consonant
+"""
+x=str(input())
+if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U'):
+    print(f'{x} is a vowel')
+else:
+    print(f'{x} is consonant')   
+"""
+#check whether the number is multiple of 3 or 7
+"""
+n=int(input())
+if (n%3==0):
+    print(f'{n} is multiple of 3 ')
+elif (n%7==0):
+    print(f'{n} is multiple of 7')
+else:
+    print(f'{n} is not multiple of 3 or 7')
+"""
+#check whether the character is uppercase,lowercase,digit,special
+"""
+s=str(input())
+if (s.isupper()):
+    print(f'{s} is upper')
+elif (s.islower()):
+    print(f'{s} is lower')
+elif (s.isdigit()):
+    print(f'{s} is digit')
+else:
+    print(f'{s} is special character')
+"""
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
